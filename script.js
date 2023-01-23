e2d0bdf9d85137cd0270ee103f9de9fa9d006abb
David Heger <david_heger@hotmail.de>
2023-01-23 19:22:58 +0100

    expanded upgrade & research system

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 window.onload = function () {
+  createUpgradeElements();
+  addUpgradeEventListeners();
+  createReseachElements();
+  addResearchEventListeners();
   displayUpgrades();
   displayResearches();
   if (localStorage.getItem("save data")) {
@@ -11,6 +15,7 @@ let score = 0;
 let totalScore = 0;
 let incrementPower = 1;
 let incrementPerSec = 0;
+let costReduction = 0;
 
 // Get elements
 const scoreElement = document.getElementById("score");
@@ -33,7 +38,25 @@ class Upgrade {
 
 let upgrades = [
   new Upgrade("Click Upgrade 1", 10, 1, 0),
-  new Upgrade("Auto Upgrade 1", 100, 1, 0)
+  new Upgrade("Click Upgrade 2", 100, 5, 0),
+  new Upgrade("Click Upgrade 3", 1000, 10, 0),
+  new Upgrade("Click Upgrade 4", 2500, 20, 0),
+  new Upgrade("Click Upgrade 5", 10000, 50, 0),
+  new Upgrade("Click Upgrade 6", 50000, 100, 0),
+  new Upgrade("Click Upgrade 7", 99999, 900, 0),
+  new Upgrade("Click Upgrade 8", 1000000, 1337, 0),
+  new Upgrade("Click Upgrade 9", 1050505, 5987, 0),
+  new Upgrade("Click Upgrade 10", 100000000, 10001, 0),
+  new Upgrade("Auto Upgrade 1", 100, 1, 0),
+  new Upgrade("Auto Upgrade 2", 500, 2, 0),
+  new Upgrade("Auto Upgrade 3", 1000, 5, 0),
+  new Upgrade("Auto Upgrade 4", 5000, 9, 0),
+  new Upgrade("Auto Upgrade 5", 10000, 14, 0),
+  new Upgrade("Auto Upgrade 6", 50000, 20, 0),
+  new Upgrade("Auto Upgrade 7", 100000, 100, 0),
+  new Upgrade("Auto Upgrade 8", 500000, 555, 0),
+  new Upgrade("Auto Upgrade 9", 100000, 1337, 0),
+  new Upgrade("Auto Upgrade 10", 5000000, 9999, 0),
 ];
 
 function purchaseUpgrade(upgradeName) {
@@ -42,7 +65,7 @@ function purchaseUpgrade(upgradeName) {
       if (score >= upgrade.cost) {
         score -= upgrade.cost;
         upgrade.count += 1;
-        upgrade.cost = upgradeCost(upgrade.cost, 10, upgrade.count);
+        upgrade.cost = upgradeCost(upgrade.cost, 10, upgrade.count, costReduction);
         if (upgrade.name.includes("Click")) {
           incrementPower += upgrade.power;
         } else if (upgrade.name.includes("Auto")) {
@@ -60,7 +83,6 @@ function displayUpgrades() {
     let costElement = document.getElementById(`${upgrade.name}Cost`);
     let countElement = document.getElementById(`${upgrade.name}Count`);
     let powerElement = document.getElementById(`${upgrade.name}Power`);
-    element.innerHTML = upgrade.name;
     costElement.innerHTML = "Cost: " + upgrade.cost;
     countElement.innerHTML = "Owned: " + upgrade.count;
     powerElement.innerHTML = "Power: " + upgrade.power;
@@ -72,6 +94,58 @@ function displayUpgrades() {
   }
 }
 
+function createUpgradeElement(upgrade) {
+  let li = document.createElement("li");
+
+  let button = document.createElement("button");
+  button.innerHTML = upgrade.name;
+  button.id = upgrade.name;
+  li.appendChild(button);
+
+  let cost = document.createElement("div");
+  cost.innerHTML = "Cost: " + upgrade.cost;
+  cost.className = "upgradeText";
+  cost.id = `${upgrade.name}Cost`;
+  li.appendChild(cost);
+
+  let count = document.createElement("div");
+  count.innerHTML = "Owned: " + upgrade.count;
+  count.className = "upgradeText";
+  count.id = `${upgrade.name}Count`;
+  li.appendChild(count);
+
+  let power = document.createElement("div");
+  power.innerHTML = "Power: " + upgrade.power;
+  power.className = "upgradeText";
+  power.id = `${upgrade.name}Power`;
+  li.appendChild(power);
+
+  return li;
+}
+
+function createUpgradeElements() {
+  let cUpgradeList = document.getElementById("clickUpgradeList");
+  let aUpgradeList = document.getElementById("autoUpgradeList");
+  for (let upgrade of upgrades) {
+    let upgradeElement = createUpgradeElement(upgrade);
+    if (upgrade.name.includes("Click")) {
+      cUpgradeList.appendChild(upgradeElement);
+    } else if (upgrade.name.includes("Auto")) {
+      aUpgradeList.appendChild(upgradeElement);
+    }
+
+  }
+}
+
+function addUpgradeEventListeners() {
+  for (let upgrade of upgrades) {
+    let element = document.getElementById(upgrade.name);
+    element.addEventListener("click", function () {
+      purchaseUpgrade(upgrade.name);
+      displayUpgrades();
+    });
+  }
+}
 
 /**
  * Research System
@@ -87,26 +161,27 @@ class Research {
 }
 
 let researches = [
-  new Research("Increment Power 1", 1000, 2, 0, "mul"),
-  new Research("Increment Cost 1", 10000, 10, 0, "red"),
+  new Research("Increment Click Power ", 1000, 2, 0, "mul"),
+  new Research("Increment Auto Power", 10000, 2, 0, "mul"),
+  new Research("Cost Reduction", 10000, 10, 0, "red"),
 ];
 
-function purchaseResearch(researchName, upgradeName) {
+function purchaseResearch(researchName) {
   for (let research of researches) {
     if (research.name === researchName) {
       if (totalScore >= research.cost) {
         research.count += 1;
         research.cost = researchCost(research.cost);
-        for (let upgrade of upgrades) {
-          if (upgrade.name === upgradeName) {
-            if (research.type === "mul") {
-              upgrade.power *= research.power;
-            } else if (research.type === "red") {
-              upgrade.cost *= (1 - research.power);
-            }
-            break;
+        if (research.type === "mul") {
+          if (research.name.includes("Click")) {
+            incrementPower *= research.power;
+          } else if (research.name.includes("Auto")) {
+            incrementPerSec *= research.power;
           }
+        } else if (research.type === "red") {
+          costReduction *= (1 - research.power);
         }
+
         break;
       }
     }
@@ -119,12 +194,9 @@ function displayResearches() {
     let costElement = document.getElementById(`${research.name}Cost`);
     let countElement = document.getElementById(`${research.name}Count`);
     let powerElement = document.getElementById(`${research.name}Power`);
-    let typeElement = document.getElementById(`${research.name}Type`);
-    element.innerHTML = research.name;
     costElement.innerHTML = "Cost: " + research.cost;
     countElement.innerHTML = "Owned: " + research.count;
     powerElement.innerHTML = "Power: " + research.power;
-    typeElement.innerHTML = "Type: " + research.type;
     if (totalScore >= research.cost) {
       element.disabled = false;
     } else {
@@ -133,12 +205,62 @@ function displayResearches() {
   }
 }
 
+function createResearchElement(research) {
+  let li = document.createElement("li");
 
-const reloadUI = setInterval(function () {
+  let button = document.createElement("button");
+  button.innerHTML = research.name;
+  button.id = research.name;
+  li.appendChild(button);
+
+  let cost = document.createElement("div");
+  cost.innerHTML = "Cost: " + research.cost;
+  cost.className = "upgradeText";
+  cost.id = `${research.name}Cost`;
+  li.appendChild(cost);
+
+  let count = document.createElement("div");
+  count.innerHTML = "Owned: " + research.count;
+  count.className = "upgradeText";
+  count.id = `${research.name}Count`;
+  li.appendChild(count);
+
+  let power = document.createElement("div");
+  power.innerHTML = "Power: " + research.power;
+  power.className = "upgradeText";
+  power.id = `${research.name}Power`;
+  li.appendChild(power);
+
+  return li;
+}
+
+function createReseachElements() {
+  let researchList = document.getElementById("researchList");
+  for (let research of researches) {
+    let researchElement = createResearchElement(research);
+    researchList.appendChild(researchElement);
+  }
+}
+
+function addResearchEventListeners() {
+  for (let research of researches) {
+    let element = document.getElementById(research.name);
+    element.addEventListener("click", function () {
+      purchaseResearch(research.name);
+      displayResearches();
+    });
+  }
+}
+
+function updateScore() {
   scoreElement.innerHTML = "Score: " + score;
   totalScoreElement.innerHTML = "Total Score: " + totalScore;
   incrementScoreElement.innerHTML = "Points per Increment: " + incrementPower;
   incrementPerSecScoreElement.innerHTML = "Increment Per Secound: " + incrementPerSec;
+}
+
+const reloadUI = setInterval(function () {
+  updateScore();
   displayUpgrades();
   displayResearches();
 }, 100);
@@ -189,13 +311,13 @@ function load() {
 }
 
 // Simple upgradeCost calculation
-function upgradeCost(cost, div, count) {
-  return Math.ceil(cost + ((cost / div) + count) * (count * 1.025));
+function upgradeCost(cost, div, count, costReduction) {
+  return Math.ceil((cost + ((cost / div) + count) * (count * 1.025)) - (10 * costReduction));
 }
 
 // Simple researchCost calculation
 function researchCost(cost) {
-  cost *= 5;
+  return cost *= 5;
 }
 
 /**
@@ -215,42 +337,10 @@ function autoIncrement() {
 const incrementButton = document.getElementById("increment");
 incrementButton.addEventListener("click", function () {
   score += incrementPower;
+  totalScore += incrementPower;
 });
 
 const saveButton = document.getElementById("save");
 saveButton.addEventListener("click", function () {
   save();
-});
-
-/**
- * Click Upgrades
- */
-const c1 = document.getElementById("Click Upgrade 1");
-c1.addEventListener("click", function () {
-  purchaseUpgrade("Click Upgrade 1");
-  displayUpgrades();
-});
-
-/**
- * Auto Upgrades
- */
-const a1 = document.getElementById("Auto Upgrade 1");
-a1.addEventListener("click", function () {
-  purchaseUpgrade("Auto Upgrade 1");
-  displayUpgrades();
-});
-
-/**
- * Research
- */
-const r1 = document.getElementById("Increment Power 1");
-r1.addEventListener("click", function () {
-  purchaseResearch("Increment Power", "Click Upgrade 1");
-  displayResearches();
-});
-
-const r2 = document.getElementById("Increment Cost 1");
-r2.addEventListener("click", function () {
-  purchaseResearch("Increment Cost", "Click Upgrade 1");
-  displayResearches();
 });
